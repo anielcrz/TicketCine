7e01b22eca17c4d236610bc70e163471147ee349
Igor Lisboa <igorlisboa30@hotmail.com>
2023-04-09 09:31:34 -0300

    Mensagem de conflito resolvida ao realizar migração para o banco de dados.

--- TicketCine/Program.cs
+++ TicketCine/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddEntityFrameworkNpgsql()
+builder.Services
     .AddDbContext<TicketCineContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("TicketCineDB")));
         
 
