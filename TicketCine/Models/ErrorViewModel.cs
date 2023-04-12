63032bfd70f21d03bb1fafa088a6bf017f4ae778
Aniel Cruz <aniel.bispo@gmail.com>
2023-04-11 23:06:43 -0300

    remocao do MVC

--- TicketCine/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TicketCine.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
